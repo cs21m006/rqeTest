853524359a21ad496e980d172249f880c4557bcb
ivonne <wi18b042@technikum-wien.at>
2019-10-11 23:20:57 +0200

    1st entity for hotel management system

---
+++ Buchung.java
@@ -0,0 +1,14 @@
+public class Buchung extends HotelDokumentation; 
+public int nummer; 
+public String hotel; 
+public String Straße; 
+public String Ort; 
+public int PLZ; 
+
+public Buchung (int nummer, String hotel, String straße, String ort, int plz){
+this.nummer=nummer; 
+this.hotel=hotel; 
+this.straße=straße; 
+this.ort=ort;
+this.plz=plz;
+};
